d2f1bd277f9a74b16c9fa5379655748a6ae7d45c
MaggieWesolowska <maggiewes07@gmail.com>
2023-07-11 15:41:35 +0200

    added pages to main file

---
+++ src/pages/Phonebook.js
@@ -0,0 +1,21 @@
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+
+import ContactForm from '../components/ContactForm/ContactForm';
+import ContactList from '../components/ContactList/ContactList';
+import Filter from '../components/Filter/Filter';
+
+export const Phonebook = () => {
+  return (
+    <>
+      <HelmetProvider>
+        <Helmet>
+          <title>Phonebook</title>
+        </Helmet>
+        <ContactForm />
+        <h2>Contacts</h2>
+        <Filter />
+        <ContactList />
+      </HelmetProvider>
+    </>
+  );
+};
