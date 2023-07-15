fdc5ea70d0bc078683f5cf9a266355e969394ced
MaggieWesolowska <maggiewes07@gmail.com>
2023-07-15 14:49:06 +0200

    added styles to pages

--- src/components/AuthNav/AuthNav.jsx
+++ src/components/AuthNav/AuthNav.jsx
@@ -4,10 +4,10 @@ import css from './AuthNav.module.css';
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink className={css.link} to='/register'>
+      <NavLink className={css.linkReg} to='/register'>
         Register
       </NavLink>
-      <NavLink className={css.link} to='/login'>
+      <NavLink className={css.linkLog} to='/login'>
         Log In
       </NavLink>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,26 +1,38 @@
+import {
+  FcBusinessContact,
+  FcCellPhone,
+  FcConferenceCall,
+  FcAlarmClock,
+} from 'react-icons/fc';
+
 const styles = {
   container: {
-    minHeight: 'calc(100vh - 50px)',
+    minHeight: 'calc(50vh - 50px)',
+    minWidth: 'calc(50vw - 50px)',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    flexDirection: 'column',
   },
   title: {
-    fontWeight: 500,
-    fontSize: 48,
+    fontWeight: 700,
+    fontSize: 40,
     textAlign: 'center',
+    marginBottom: 20,
+    color: '#333',
   },
 };
 
 export default function Home() {
   return (
     <div style={styles.container}>
-      <h1 style={styles.title}>
-        Phonebook Home Page{' '}
-        <span role='img' aria-label='Greeting icon'>
-          💁‍♀️
-        </span>
-      </h1>
+      <h1 style={styles.title}>Welcome to Phonebook </h1>
+      <p>
+        <FcAlarmClock size='75' />
+        <FcCellPhone size='75' />
+        <FcConferenceCall size='75' />
+        <FcBusinessContact size='75' />
+      </p>
     </div>
   );
 }
