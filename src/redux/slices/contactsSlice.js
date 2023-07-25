f19b882053e291db4061e506832680f6e3cb9d74
MaggieWesolowska <maggiewes07@gmail.com>
2023-07-25 13:36:01 +0200

    edited slice for addCase in contacts

--- src/redux/slices/contactsSlice.js
+++ src/redux/slices/contactsSlice.js
@@ -23,101 +23,100 @@ const contactsSlice = createSlice({
     error: null,
   },
   reducers: {},
-  // extraReducers: builder => {
-  //   builder
-  //     .addCase(fetchContacts.pending, () => {
-  //       handlePending;
-  //     })
-  //     .addCase(fetchContacts.rejected, () => {
-  //       handleRejected;
-  //     })
-  //     .addCase(fetchContacts.fulfilled, (state, action) =>
-  //       state.isLoading = false;
-  //       state.error = null;
-  //       state.contacts = action.payload;
-  //     })
-  //     .addCase(addContact.pending, () => {
-  //       handlePending;
-  //     })
-  //     .addCase(addContact.rejected, () => {
-  //       handleRejected;
-  //     })
-  //     .addCase(addContact.fulfilled, (state, action) => {
-  //       state.isLoading = false;
-  //       state.error = null;
-  //       state.contacts = action.payload;
-  //     })
-  //     .addCase(deleteContact.pending, () => {
-  //       handlePending;
-  //     })
-  //     .addCase(deleteContact.rejected, () => {
-  //       handleRejected;
-  //     })
-  //     .addCase(deleteContact.fulfilled, (state, action) =>
-  //       state.isLoading = false;
-  //       state.error = null;
-  //       const { id } = action.payload;
-  //       state.contacts = state.contacts.filter(
-  //         contact => contact.id !== id
-  //       );
-  //     })
-  //     .addCase(editContact.pending, () => {
-  //       handlePending;
-  //     })
-  //     .addCase(editContact.rejected, () => {
-  //       handleRejected;
-  //     })
-  //     .addCase(editContact.fulfilled, (state, action) => {
-  //       state.isLoading = false;
-  //       state.error = null;
-  //       const { id, name, number } = action.payload;
-  //       let contactEdit = state.contacts.find(
-  //         contact => contact.id === id
-  //       );
-  //       contactEdit.name = name;
-  //       contactEdit.number = number;
-  //     });
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, () => {
+        handlePending;
+      })
+      .addCase(fetchContacts.rejected, () => {
+        handleRejected;
+      })
+      .addCase(fetchContacts.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.contacts = action.payload;
+      })
+      .addCase(addContact.pending, () => {
+        handlePending;
+      })
+      .addCase(addContact.rejected, () => {
+        handleRejected;
+      })
+      .addCase(addContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        state.contacts.push(action.payload);
+      })
+      .addCase(deleteContact.pending, () => {
+        handlePending;
+      })
+      .addCase(deleteContact.rejected, () => {
+        handleRejected;
+      })
+      .addCase(deleteContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const { id } = action.payload;
+        state.contacts = state.contacts.filter(
+          contact => contact.id !== id
+        );
+      })
+      .addCase(editContact.pending, () => {
+        handlePending;
+      })
+      .addCase(editContact.rejected, () => {
+        handleRejected;
+      })
+      .addCase(editContact.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.error = null;
+        const { id, name, number } = action.payload;
+        let contactEdit = state.contacts.find(
+          contact => contact.id === id
+        );
+        contactEdit.name = name;
+        contactEdit.number = number;
+      });
 
-  extraReducers: {
-    [fetchContacts.pending]: handlePending,
-    [fetchContacts.rejected]: handleRejected,
-    [fetchContacts.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.contacts = action.payload;
-    },
+    // [fetchContacts.pending]: handlePending,
+    // [fetchContacts.rejected]: handleRejected,
+    // [fetchContacts.fulfilled](state, action) {
+    //   state.isLoading = false;
+    //   state.error = null;
+    //   state.contacts = action.payload;
+    // },
 
-    [addContact.pending]: handlePending,
-    [addContact.rejected]: handleRejected,
-    [addContact.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      state.contacts.push(action.payload);
-    },
+    // [addContact.pending]: handlePending,
+    // [addContact.rejected]: handleRejected,
+    // [addContact.fulfilled](state, action) {
+    //   state.isLoading = false;
+    //   state.error = null;
+    //   state.contacts.push(action.payload);
+    // },
 
-    [deleteContact.pending]: handlePending,
-    [deleteContact.rejected]: handleRejected,
-    [deleteContact.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const { id } = action.payload;
-      state.contacts = state.contacts.filter(
-        contact => contact.id !== id
-      );
-    },
+    // [deleteContact.pending]: handlePending,
+    // [deleteContact.rejected]: handleRejected,
+    // [deleteContact.fulfilled](state, action) {
+    //   state.isLoading = false;
+    //   state.error = null;
+    //   const { id } = action.payload;
+    //   state.contacts = state.contacts.filter(
+    //     contact => contact.id !== id
+    //   );
+    // },
 
-    [editContact.pending]: handlePending,
-    [editContact.rejected]: handleRejected,
-    [editContact.fulfilled](state, action) {
-      state.isLoading = false;
-      state.error = null;
-      const { id, name, number } = action.payload;
-      let contactEdit = state.contacts.find(
-        contact => contact.id === id
-      );
-      contactEdit.name = name;
-      contactEdit.number = number;
-    },
+    // [editContact.pending]: handlePending,
+    // [editContact.rejected]: handleRejected,
+    // [editContact.fulfilled](state, action) {
+    //   state.isLoading = false;
+    //   state.error = null;
+    //   const { id, name, number } = action.payload;
+    //   let contactEdit = state.contacts.find(
+    //     contact => contact.id === id
+    //   );
+    //   contactEdit.name = name;
+    //   contactEdit.number = number;
+    // },
   },
 });
 
